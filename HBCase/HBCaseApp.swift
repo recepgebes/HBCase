646ce683663fb4b659cb4b57f887e0808c253618
Recep Gebeş <recep.gebes@ibtech.com.tr>
2022-04-25 13:41:58 +0300

    Initial Commit

---
+++ HBCase/HBCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HBCaseApp.swift
+//  HBCase
+//
+//  Created by Recep on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HBCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
